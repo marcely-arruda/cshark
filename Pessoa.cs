f75cdb49473c7023c4b92d8a9604dea4590364c0
marcely-arruda <arrudamarcely460@gmail.com>
2024-04-02 08:48:06 -0400

    adicionado exemplo de classe pessoa

---
+++ Pessoa.cs
@@ -0,0 +1,24 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String cpf;
+    
+    public Pessoa(string nome,int idade,string cpf) {
+        Nome = nome;
+        Idade = idade;
+        CPF = cpf;
+    }
+
+    public string Nome {
+        get{return nome;}
+        set{nome=value;} 
+    }
+    public int Idade {
+        get {return idade;}
+        set {idade value}
+    }
+    public string CPF {
+        get (return cpf;)
+        set (cpf = value;)
+    }
+}
